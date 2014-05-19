3848aac0d38c663815caeaf7ecc6789772bda9dd
Zolnai Dániel <zoiddani@gmail.com>
2014-05-19 22:43:20 +0200

    Starting project

---
+++ GithubClient/GithubClient/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GithubClient.Resources;
+
+namespace GithubClient
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
